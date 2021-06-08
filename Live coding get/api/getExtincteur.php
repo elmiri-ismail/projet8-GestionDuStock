ed7877dfdfa9e933b78082848595527e8ebfd4a2
Miri Ismail <ismailmiri@gmail.com>
2021-06-08 16:44:20 +0100

    live coding get

---
+++ Live coding get/api/getExtincteur.php
@@ -0,0 +1,7 @@
+<?php
+$dbh = new PDO("mysql:host=localhost;dbname=tableaux","root","Miriismail2002");
+$sql = " SELECT * FROM exproduit ";
+$extincteurQuery = $dbh->query($sql);
+$getExtincteur = $extincteurQuery->fetchAll(PDO::FETCH_ASSOC);
+print_r(json_encode($getExtincteur));
+?>
